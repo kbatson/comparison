9140cb81b2bacd232eb85b8e5aa5842e79df6b42
kbatson <kbatson@me.com>
2017-12-15 09:31:34 -0600

    Added ability to have multiple separate comparisions

--- comparison.js
+++ comparison.js
@@ -1,42 +1,57 @@
 $(document).ready(function(){
-	var comparePosition = 0;
+	var debug = true;
+	var comparePosition = {}; //Object to hold each comparison value
 	var containerWidth = $('.compareContainer').width();
 	var compareContainer = $('.compareContainer');
-	var controlsContainer, controlsDivider, controlsHandle;
+	var controlsContainer,
+		controlsDivider,
+		controlsHandle,
+		redrawInterval;
 	var createControls = function(){
 		controlsContainer = $('<div class="controlsContainer"></div>').appendTo(compareContainer);
 		controlsDivider = $('<div class="controlsDivider"></div>').appendTo(compareContainer);
 		controlsHandle = $('<button class="controlsHandle">Handle</button>').appendTo(controlsContainer);
 	}
 	
-	var  createElements = function(){
-		compareContainer.find('img').each(function(index){
-			$(this).wrap('<div class="compareImages"></div>');
-			if(index == 0){
-				$(this).parent().addClass('compareLeft');
-			}
+	var createElements = function(){
+		$(compareContainer).each(function(){
+			$(this).find('img').each(function(index){
+				$(this).wrap('<div class="compareImages"></div>');
+				if(index == 0){
+					$(this).parent().addClass('compareLeft');
+				}
+			}).append();
 		});
+		if(debug){
+			$(compareContainer).each(function(){
+				$(this).append('<p class="display"></p>');
+			});
+		}
 	}
 	createControls();
 	createElements();
 	
-	$('.compareContainer').mousemove(function(e){
-		comparePosition = e.pageX; //Need to isolate this to specific dimensions
-	    $('#display').html("X Axis : " + e.pageX + " Y Axis : " + e.pageY + " comparePosition: " + comparePosition);
+	$('.compareContainer').each(function(index, element){
+		$(this).mousemove(function(e){
+			comparePosition[index] = e.pageX; //Need to isolate this to specific dimensions
+		    $(this).find('.display').html("X Axis : " + e.pageX + " Y Axis : " + e.pageY + " comparePosition: " + comparePosition[index]);
+		});
+		
+		
+		$(element).find('.controlsHandle').on('mousedown', function(event){
+			redrawInterval = setInterval(function(){
+				if(comparePosition[index] < containerWidth && comparePosition[index] > 0){
+					$(element).find('.compareLeft').width(comparePosition[index]);
+					$(element).find('.controlsContainer, .controlsDivider').css('left',comparePosition[index]);
+				}
+			}, 50);	
+		}).on('mouseup', function(event){
+			clearInterval(redrawInterval);
+		});
 	});
 	
-	var int00;
-	$('.controlsHandle').on('mousedown', function(event){
-		int00 = setInterval(function(){
-			if(comparePosition < containerWidth && comparePosition > 0){
-				$('.compareLeft').width(comparePosition);
-				$('.controlsContainer, .controlsDivider').css('left',comparePosition);
-			}
-		}, 50);	
-	}).on('mouseup', function(event){
-		clearInterval(int00);
-	});
+	/* Stop dragging when mouseup occurs on other elements to prevent weird behavior */
 	$('body').on('mouseup', function(event){
-		clearInterval(int00);
+		clearInterval(redrawInterval);
 	})
 });
